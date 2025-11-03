ec2889e1a29f55882a34bc3950651356642fea7f
Will Schuckenbrock <wischuckenbrock@davidson.edu>
2025-11-03 10:52:40 -0500

    CardParser to main

--- Assets/Scripts/CardLoader.cs
+++ Assets/Scripts/CardLoader.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 
 public class CardLoader : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         List<CardData> cards = CardParser.ParseCsv();
         Debug.Log($"Loaded {cards.Count} cards.");
     }
 
-    // Update is called once per frame
     void Update()
     {
         
